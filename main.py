c28c88635f3e4eadb61d7137407aa6e1a3fb58a3
gersonUb <gersoncabj@gmail.com>
2025-10-22 05:11:45 -0300

    feat: implement main py

---
+++ main.py
@@ -0,0 +1,17 @@
+from src.core.project_creator import ProjectCreator
+from src.core.infraestructure.file_manager import FileManager
+from src.cli.questionnaire import ask_project_config
+
+
+def main():
+    config = ask_project_config()
+    file_manager = FileManager(config.name)
+    creator = ProjectCreator(file_manager)
+    
+    creator.create_project(config)
+    
+    print(f"\n✅ Project '{config.name}' create correctly!")
+
+
+if __name__ == "__main__":
+    main()
