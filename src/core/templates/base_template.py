62eef1cf3b7d07f21e01b149311534c739c23067
gersonUb <gersoncabj@gmail.com>
2025-10-22 05:12:07 -0300

    feat: create template base

---
+++ src/core/templates/base_template.py
@@ -0,0 +1,9 @@
+from typing import Dict, Any
+
+def base_template(project_name: str) -> Dict[str, Any]:
+    return {
+        project_name: {
+            "frontend": None,
+            "domain": None,
+        }
+    }
